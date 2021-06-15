fc703cf8d304118bc6a35ea6a8600d7896b21bcf
Drew Beckley <85914430+DrewBeckley@users.noreply.github.com>
2021-06-15 02:57:56 +0000

    Shifted things around and removed some more old methods

--- OldInvProject/inventory.java
+++ OldInvProject/inventory.java
@@ -5,9 +5,7 @@ import java.util.stream.*;
 import java.util.function.*;
 import java.util.*;
 public class inventory extends ConsoleProgram implements Runnable 
-{
-	//Arrays here
-	
+{	
 	public final String[] Cammands={"Add", "Remove", "Entries","Entry","ammounts","ammount","all cammands","change","log","check","inv",};
 	public String[] testing={"joe","bob","moe","john", "Drew","Tom"};
 
@@ -25,6 +23,39 @@ public class inventory extends ConsoleProgram implements Runnable
 		Optional: Look into immutablity/pure functions
 	*/
 	//was 330 some lines before 
+
+
+
+//printing methods********************************************************************
+//now it is the suppliers/consumers then use apply on the print consumer I had
+
+
+	public Supplier<String> CammandsList=()->Arrays.stream(Cammands).collect(Collectors.joining(", ","The cammands are:\n",""));
+
+	public Supplier<String> IBDcall1=()->"\n".repeat(10)+CammandsList.get();
+	
+	
+	public Supplier<String> CurrentInventory=()->{
+		var buffer=new StringBuffer();
+		int size=entriesA.size();
+		buffer.append(size+" items in the system\n");
+		int i=0;
+		for (Entries n:entriesA)
+		{
+			buffer.append(n+utilityPrint.flatline(19,"_")+Entries.amountsArray.get(i)+"\n");
+			i++;
+		}
+		return buffer.toString();
+	};
+	
+	
+	
+	//c and p template
+	//public Supplier<String> undefined=()->
+
+	private Consumer<String>print=(text)->System.out.println(text);
+
+	public Supplier<String> TBD=()->"not implimented yet";
 	
 	public void run()
 	{
@@ -43,19 +74,17 @@ public class inventory extends ConsoleProgram implements Runnable
 
 		print.accept(CurrentInventory.get());
 		//System.out.print(IBDcall1);
-		//invcall();
-		//I beleive this b
 		while(true)
 		{
 			String ok=readLine("what do you want me to do: ");
 			Checker(ok);
-			invcall();
+			print.accept(CurrentInventory.get());
 		}
 	}
 
 
 
-
+	//TBD are not impelimented yep
 	public void Checker(String tester)
 	{
 		
@@ -67,12 +96,12 @@ public class inventory extends ConsoleProgram implements Runnable
 		//could shift printing out
 		var  linkedActions=new HashMap<String, Supplier<String>>();
 		
-		//TBD are not impelimented yep
+		
 		linkedActions.put("IBD",IBDcall1);
 		linkedActions.put("logbook",TBD);
 
 		var x=linkedActions.containsKey(tester);
-		//do the
+		//"invoke" the supplier 
 		if(x){
 			linkedActions.get(tester).get();
 		}
@@ -107,26 +136,10 @@ public class inventory extends ConsoleProgram implements Runnable
 			call(i,tester);
 		}
 	}
-		/*
-		for(int i=0; i<Cammands.length;i++)
-		{
-			if(cammandCheck(tester,i))
-			{
-				//System.out.println(tester);//pasted
-				//System.out.println(data.Cammands[i]);//pasted
-				call(i,tester);
-				break;
-			}
-		}
-		//*/
+		
 	
 
-	// @Deprecated
-	// public boolean cammandCheck(String camm,int j)
-	// {
-	// 	return camm.equalsIgnoreCase(Cammands[j]);
-	// }
-	//cammandCheck(tester,i)
+	
 
 	//use the same trick  with hashmap
 	
@@ -170,56 +183,10 @@ public class inventory extends ConsoleProgram implements Runnable
 				break;
 		};
 	}
-//printing methods********************************************************************
-//now it is the suppliers/consumers then use apply on the 
 
-
-	public Supplier<String> CammandsList=()->Arrays.stream(Cammands).collect(Collectors.joining(", ","The cammands are:\n",""));
-
-	public Supplier<String> IBDcall1=()->"\n".repeat(10)+CammandsList.get();
-	
-	
-	public Supplier<String> CurrentInventory=()->{
-		var buffer=new StringBuffer();
-		int size=entriesA.size();
-		buffer.append(size+" items in the system\n");
-
-		
-		int i=0;
-		for (Entries n:entriesA)
-		{
-			buffer.append(n+utilityPrint.flatline(19,"_")+Entries.amountsArray.get(i)+"\n");
-			i++;
-		}
-		return buffer.toString();
-	};
 	
 	
 	
-	
-	//public Supplier<String> undefined=()->
-
-	private Consumer<String>print=(text)->System.out.println(text);
-
-	public Supplier<String> TBD=()->"not implimented yet";
-	
-	
-	
-
-	public void invcall()
-	{
-		
-		//System.out.println("log this");
-		int size=entriesA.size();
-		print.accept(size+" items in the system");
-		
-		int i=0;
-		for (Entries n:entriesA)
-		{
-			System.out.println(n+utilityPrint.flatline(19,"_")+Entries.amountsArray.get(i));
-			i++;
-		}
-	}
 	
 
 	
@@ -294,56 +261,5 @@ public class inventory extends ConsoleProgram implements Runnable
 	}
 	*/
 }
-
-	/*
-	public static ArrayList <String>Entries=new ArrayList<String>();
 	
 	
-	public static ArrayList <Double> amounts=new ArrayList<Double>();/*
-	public static void logBook()
-	{
-		
-		
-	}
-	*/
-	//testing for x
-	//on remove list replaced by anyMatch
-	// @Deprecated
-	// //utility method
-	// public int testFor(String  test,String[] to)
-	// {
-	// 	//System.out.println(test+to);
-	// 	int i=1;
-	// 	while (i <to.length)
-	// 	{
-	// 		if (to[i-1].equalsIgnoreCase(test))
-	// 		{
-	// 			return i;
-	// // 		}
-	// // 		else 
-	// // 		{
-	// // 			i = i + 1;
-	// // 		}
-	// // 	}
-		
-		
-	// 	return -1;
-	// }  
-	//@Deprecated 
-	// public int testFor(String  test, ArrayList<String> to)
-	// {
-	// 	//System.out.println(test+to);
-	// 	int i=0;
-	// 	while (i <to.size())
-	// 	{
-	// 		if (to.get(i).equalsIgnoreCase(test))
-	// 		{
-	// 			return i;
-	// 		}
-	// 		else 
-	// 		{
-	// 			i = i + 1;
-	// 		}
-	// 	}
-	// 	return -1;
-	// } 
